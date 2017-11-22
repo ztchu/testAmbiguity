f12bf7d5f3b0d185c452e207f3387090e8e5aa7f
chuzhongtao <chuzhongtao@cvte.com>
2017-11-22 09:10:43 +0800

    deleted ptr is unknown, when deleted again , dump will occur

--- testAmbiguity/testAmbiguity/dump.cpp
+++ testAmbiguity/testAmbiguity/dump.cpp
@@ -1,4 +1,5 @@
 #include "dump.h"
+#include <iostream>
 
 void TestCreateDumpFile()
 {
@@ -10,6 +11,14 @@ void TestDeleteNullptr()
 {
     int *ptr = new int(4);
     delete ptr;
-    ptr = nullptr;
-    delete ptr;
+    //ptr = nullptr;
+    //delete ptr;
+    if (ptr == nullptr)
+    {
+        std::cout << "ptr is nullptr when deleted." << std::endl;
+    }
+    else
+    {
+        std::cout << "ptr is unknown when deleted." << std::endl;
+    }
 }
